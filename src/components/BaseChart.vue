c78b92026105b5963395c3f37dd514d3da63de4d
jiyuuki <salma.hajrawi@gmail.com>
2022-08-15 21:41:49 +0200

    install echarts package and init BaseChart component

---
+++ src/components/BaseChart.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div
+      class="space-y-3 chart"
+      id="main"
+    />
+  </div>
+</template>
+
+<script>
+import * as echarts from 'echarts'
+
+export default {
+  name: 'BaseChart',
+
+  data: () => {
+    return {
+      myChart: ''
+    }
+  },
+
+  methods: {
+    initChart(id) {
+      this.myChart = echarts.init(document.getElementById(id))
+
+      this.myChart.setOption({
+        title: {
+          text: 'ECharts Getting Started Example'
+        },
+        tooltip: {},
+        xAxis: {
+          data: ['shirt', 'cardigan', 'chiffon', 'pants', 'heels', 'socks']
+        },
+        yAxis: {},
+        series: [
+          {
+            name: 'sales',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+          }
+        ]
+      });
+    }
+  },
+
+  mounted() {
+    this.initChart('main')
+  }
+}
+</script>
+
+<style scoped>
+.chart {
+  width: 600px;
+  height:400px;
+}
+</style>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,13 +1,27 @@
 <template>
-  <div>
-    DashboardView
+  <div class="container px-6 py-10 mx-auto">
+    <h1 class="text-3xl font-semibold text-gray-800 capitalize lg:text-4xl">Welcome to our <br>Dashboard</h1>
+
+    <div class="mt-2">
+      <span class="inline-block w-40 h-1 rounded-full bg-blue-500" />
+      <span class="inline-block w-3 h-1 ml-1 rounded-full bg-blue-500" />
+      <span class="inline-block w-1 h-1 ml-1 rounded-full bg-blue-500" />
+    </div>
+
+    <div class="flex flex-wrap jus mt-20">
+      <BaseChart />
+    </div>
   </div>
 </template>
 
 <script>
+import BaseChart from '@/components/BaseChart.vue';
 
 export default {
   name: 'DashboardView',
 
+  components: {
+    BaseChart,
+  }
 }
 </script>
