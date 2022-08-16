78f187da6e147f2ab157095d371d42465543472a
Salma EL HAJRAOUI <11775487+jiyuuki@users.noreply.github.com>
2022-08-16 17:18:38 +0200

    navbar (#6)
    * navbar
    
    * clear tailwind classes

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    NavBar
+    <nav class="bg-white shadow dark:bg-gray-800">
+      <div class="items-left justify-left p-6 mx-auto text-gray-600 capitalize dark:text-gray-300">
+        <router-link
+          :to="{ name: 'DashboardView' }"
+          class="text-gray-800 transition-colors duration-200 transform dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6"
+        >
+          Dashboard
+        </router-link>
+      </div>
+    </nav>
   </div>
 </template>
 
